f991b5b88bee368f4e884fa22ff773f59b368625
Jaime G. Wong <j@jgwong.org>
2023-08-26 16:55:53 -0500

    Remove JG layer in favor of PC layer
    I want to use the hardware "Windows/PC" button instead of layers, since
    I won't be using Bluetooth to switch between computers.

--- keyboards/keychron/k8_pro/ansi/white/keymaps/jgwong/keymap.c
+++ keyboards/keychron/k8_pro/ansi/white/keymaps/jgwong/keymap.c
@@ -19,12 +19,14 @@
 // clang-format off
 enum layers {
   PC_BASE = 0,
-  JG_BASE = 2,
-  MAC_BASE,
+  MAC_BASE = 2,
   FN,
   MOVE,
   MOUSE
 };
+
+// Key aliases for readability
+#define K_MOVE LT(MOVE, KC_CAPS)
 
 // Combos
 enum combos {
@@ -105,18 +107,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,   KC_CTANA, BL_STEP,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+    K_MOVE,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,             KC_UP,
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(FN),    KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT
-),
-
-[JG_BASE] = LAYOUT_tkl_ansi(
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            K_JG_B,   K_MAC_B,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    K_MOVE,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-    _______,  _______,  _______,                                _______,                                _______,  _______,  MO(FN),   _______,  _______,  _______,  _______
 ),
 
 [MAC_BASE] = LAYOUT_tkl_ansi(
@@ -147,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [MOUSE] = LAYOUT_tkl_ansi(
-    K_JG_B,
+    K_PC_B,
               _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BTN2,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_WH_U,  _______,  _______,  _______,  _______,  _______,  KC_BTN3,  _______,  _______,  _______,
@@ -158,16 +151,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool led_matrix_indicators_user(void) {
-    if (layer_state_is(JG_BASE)) {
-        led_matrix_set_value(47, 128);
-    }
-    if (layer_state_is(MAC_BASE)) {
-        led_matrix_set_value(48, 128);
-    }
-    return false;
-}
-
-void keyboard_post_init_user(void) {
-    layer_move(JG_BASE);
-}
