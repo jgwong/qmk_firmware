f53cf0182c7992b5f5a558bd50a4d3e4daba86a6
Jaime G. Wong <j@jgwong.org>
2023-08-26 17:47:38 -0500

    Redefine MOVE layer as FN2 and dedicated layer

--- keyboards/keychron/k8_pro/ansi/white/keymaps/jgwong/keymap.c
+++ keyboards/keychron/k8_pro/ansi/white/keymaps/jgwong/keymap.c
@@ -21,13 +21,18 @@ enum layers {
   PC_BASE = 0,
   MAC_BASE = 2,
   FN,
+  FN2,
   MOVE,
   MOUSE,
   MEDIA
 };
 
 // Key aliases for readability
-#define K_MOVE LT(MOVE, KC_CAPS)
+#define K_FN2 LT(FN2, KC_CAPS)
+// Browser tab previous
+#define K_BROW_P S(C(KC_TAB))
+// Browser tab next
+#define K_BROW_N C(KC_TAB)
 
 // Combos
 enum combos {
@@ -108,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             MO(MEDIA), KC_CTANA, BL_STEP,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
-    K_MOVE,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+    K_FN2,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,             KC_UP,
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(FN),    KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT
 ),
@@ -131,12 +136,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______
 ),
 
-[MOVE] = LAYOUT_tkl_ansi(
+[FN2] = LAYOUT_tkl_ansi(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_HOME,  KC_END,   _______,  K_BROW_P, K_BROW_N, _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  TO(MOVE), TO(MOUSE),_______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,            _______,
+    _______,            _______,  _______,  _______,  _______,  _______,  KC_PGDN,  KC_INS,   KC_DEL,   _______,  _______,            _______,            _______,
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______
+),
+
+[MOVE] = LAYOUT_tkl_ansi(
+    TG(MOVE), _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  TO(MOUSE),_______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,            _______,
-    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PGUP,  KC_HOME,  KC_END,   _______,  K_BROW_P, K_BROW_N, _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, _______,  _______,            _______,
+    _______,            _______,  _______,  _______,  _______,  _______,  KC_PGDN,  _______,  _______,  _______,  _______,            _______,            _______,
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______
 ),
 
