132a01b5aa40bafd5284befc596f3e0a69ba3a0d
Jaime G. Wong <j@jgwong.org>
2024-01-10 10:28:05 -0500

    Implement Mouse Keys in Accelerated mode (default)

--- keyboards/keychron/k8_pro/ansi/white/config.h
+++ keyboards/keychron/k8_pro/ansi/white/config.h
@@ -45,17 +45,9 @@
 
 #endif
 
-// Constant Mouse keys settings
-#define MK_3_SPEED
-
-#define MK_C_OFFSET_0 1
-#define MK_C_INTERVAL_0 16
-
-#define MK_C_OFFSET_1 4
-#define MK_C_INTERVAL_1 16
-
-#define MK_C_OFFSET_2 16
-#define MK_C_INTERVAL_2 16
+// Mouse keys (Accelerated) settings
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_TIME_TO_MAX 90
 
 // Settings to avoid misfiring Combos
 
