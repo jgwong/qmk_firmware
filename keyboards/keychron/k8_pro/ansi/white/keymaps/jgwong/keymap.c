d5fefd4ad0f1195cf836fdf8690051422dca4843
Jaime G. Wong <j@jgwong.org>
2024-01-10 16:26:42 -0500

    Add Square layer

--- keyboards/keychron/k8_pro/ansi/white/keymaps/jgwong/keymap.c
+++ keyboards/keychron/k8_pro/ansi/white/keymaps/jgwong/keymap.c
@@ -32,6 +32,7 @@ enum layers {
   J_OPTN, // Option layer
   J_COLON, // Colon layer
   J_TAB, // Tab layer
+  J_SQUA, // Square layer
   MOVE,
   MOUSE,
 };
@@ -40,6 +41,7 @@ enum layers {
 #define K_J_CAPS MO(J_CAPS)
 #define K_J_COLON LT(J_COLON, KC_SCLN)
 #define K_J_TAB LT(J_TAB, KC_TAB)
+#define K_J_SQUA LT(J_SQUA, KC_MPLY)
 #define K_LPAR S(KC_9) // Left parenthesis
 #define K_RPAR S(KC_0) // Right parenthesis
 #define K_LCBRC S(KC_LBRC) // Left curly bracket
@@ -130,7 +132,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [PC_BASE] = LAYOUT_tkl_ansi(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_MPLY,   KC_MUTE,  BL_TOGG,
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             K_J_SQUA,  KC_MUTE,  BL_TOGG,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
     K_J_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
     K_J_CAPS, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     K_J_COLON,KC_QUOT,            KC_ENT,
@@ -192,6 +194,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_P7,    KC_P8,    KC_P9,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_P4,    KC_P5,    KC_P6,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_P0,    KC_P1,    KC_P2,    KC_P3,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
+    _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______
+),
+
+// Square layer
+[J_SQUA] = LAYOUT_tkl_ansi(
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
     _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______
